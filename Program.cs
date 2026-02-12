0140342fb9641471691e743944ba0d7d26097190
tue-kyndal <106955975+tue-kyndal@users.noreply.github.com>
2026-02-12 20:43:48 +0000

    Update Program.cs with working code

--- Program.cs
+++ Program.cs
@@ -1,21 +1,2 @@
-using System;
-using System.Diagnostics;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        if (args.Length == 0) return;
-
-        string url = args[0];
-
-        url = url.Replace("openexplorer://", "");
-        url = url.Replace("openexplorer:", "");
-        url = url.Replace("/", "\\");
-
-        if (!url.StartsWith("\\\\"))
-            url = "\\\\" + url;
-
-        Process.Start("explorer.exe", url);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
